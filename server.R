6aa8ac1d49da37e71c1d0402079b566020031cd4
Jeffrey Bailes <jeffbailes@gmail.com>
2015-12-24 20:14:57 +1100

    Initial commit.

---
+++ server.R
@@ -0,0 +1,45 @@
+library(shiny)
+data(mtcars)
+transmission <- factor(mtcars$am, levels=c(0,1), labels=c("automatic", "manual"))
+fit <- lm(mpg ~ transmission + wt + hp + qsec, data=mtcars)
+
+# Conversion of units functions.
+kgToThouPound <- function(kg) {
+  (kg / 1000) * 2.20462
+}
+
+litreToGallon <- function(litre) {
+  0.264172 * litre
+}
+
+kwToHP <- function(kw) {
+  1.34102 * kw
+}
+
+milesTokm <- function(miles) {
+  1.60934 * miles
+}
+
+shinyServer(
+  function(input, output) {
+    prediction <- reactive({milesTokm(litreToGallon(input$petrol) * predict(fit, data.frame(transmission=input$transmission, wt=kgToThouPound(input$mass), hp=kwToHP(input$kw), qsec=input$qm)))})
+    output$petrolStationDistance <- renderPrint({input$dist})
+    output$calculatedDistance <- renderPrint({prediction()})
+    output$outText <- renderText({
+      if (prediction() > input$dist) {
+        "No problem!  You're not skrewed at all!  You can defo make it to the next petrol station."
+      } else if (-prediction() > input$dist) {
+        "You're not too skrewed.  You can make it to the next petrol station, but only if you drive in reverse the whole way."
+      } else if (input$transmission == "automatic") {
+        "You're mostly skrewed, though have you considered converting your car to manual before you set off?"
+      } else {
+        tmp <- sample(c("Take a hammer to your engine - if you reduce your car's kilowatt output, your fuel efficiency will increase!",
+                        "Accelerate slower - increasing your car's 402 metre time makes it more fuel efficient!",
+                        "Fill your car with rocks - if your car becomes heavy enough, you can just reverse your way there!",
+                        "Wait a couple of decades - your car will surely be more efficient in the future."),
+                      1)
+        paste("You're pretty skrewed, however, here is a helpful hint to maximise your chance of success:", tmp)
+    }
+    })
+  }
+)
---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Are You Skrewed?"),
+  sidebarPanel(
+    radioButtons("transmission", "Transmission:",
+                 c("Manual" = "manual",
+                   "Automatic" = "automatic")
+    ),
+    numericInput("mass", "Mass of car (kg):", 1000, min=0, max=100000, step=200),
+    numericInput("kw", "Car's Kilowatts:", 110, min=0, max=10000, step=5),
+    numericInput("qm", "Time to drive 402 metres (seconds):", 18, min=0, max=3600, step=2),
+    numericInput("dist", "Distance to the next petrol station (km):", 50, min=0, max=100000, step=25),
+    numericInput("petrol", "Litres of petrol you have in the tank:", 5, min=0, max=500, step=1)
+    ),
+  mainPanel(
+    tabsetPanel(tabPanel("Results",
+                            h4("Distance to closest petrol station (km):"),
+                            verbatimTextOutput("petrolStationDistance"),
+                            h4("How far you can drive on the current tank (km):"),
+                            verbatimTextOutput("calculatedDistance"),
+                            h4("Outcome / hints"),
+                            verbatimTextOutput("outText")),
+                tabPanel("Help", includeMarkdown('help.Rmd')))
+  )
+))
